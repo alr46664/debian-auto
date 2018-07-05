53fa0dfe2524956b7b8e00ca6e6b82429ae093b1
Andre Madureira <andreluizromano@hotmail.com>
2018-07-04 22:33:14 -0300

    fixed backup script

--- Configure Distro/backup.sh
+++ Configure Distro/backup.sh
@@ -8,10 +8,8 @@ DATE=$(date '+%F-%X' | sed -e 's@:@-@g')
 
 mount_devices(){
     local STATUS_LOCAL=0
-    NEWROOT=/new_root
-    mkdir -p "$NEWROOT" &&
-    cd "$NEWROOT" &&
-    mount -o ro "$PARTITION" "$NEWROOT" &&
+    NEWROOT=$(mount | grep "$PARTITION" | tr -s ' ' | cut -d' ' -f3)    
+    cd "$NEWROOT" &&    
     mount -o bind "$(dirname "$BACKUP_FILE")" "${NEWROOT}/media" &&    
     BACKUP_FILE=$(basename -s .tar.gz "$BACKUP_FILE") 
     STATUS_LOCAL+=$?
@@ -26,9 +24,27 @@ mount_devices(){
     return $STATUS_LOCAL
 }
 
+install_grub_efi(){
+    if ! chroot "$NEWROOT" aptitude search grub-efi-amd64 | grep -P '^i' &> /dev/null; then
+        chroot "$NEWROOT" apt-get -y install grub-efi-amd64
+    else
+        chroot "$NEWROOT" grub-install --efi-directory=/boot --target=x86_64-efi "$PARTITION"
+    fi    
+    
+}
+
+install_grub_pc(){
+    if ! chroot "$NEWROOT" aptitude search grub-pc | grep -P '^i' &> /dev/null; then
+        chroot "$NEWROOT" apt-get -y install grub-pc
+    else
+        chroot "$NEWROOT" grub-install --target=i386-pc "$PARTITION"
+    fi    
+}
+
 run_backup(){        
     chroot "$NEWROOT" \
     tar -cvpzf "/media/${BACKUP_FILE}_${DATE}.tar.gz" \
+    --numeric-owner --atime-preserve --acls --xattrs \
     --exclude=/sys \
     --exclude=/proc \
     --exclude=/dev \
@@ -43,16 +59,18 @@ run_restore(){
         return 1
     fi
     if [ -n "$EFI_BOOT" ]; then
-        chroot "$NEWROOT" \
-        grub-install --efi-directory=/boot --target=x86_64-efi "$PARTITION"
+        install_grub_efi        
     else
-        chroot "$NEWROOT" \
-        grub-install --target=i386-pc "$PARTITION"
+        install_grub_pc        
     fi        
 }
 
 show_help(){
-    echo 'Usage: backup.sh [--efi-boot EFI_PARTITION] [-b|-r] backupfile --root root_partition'
+    echo ' '
+    echo '  Usage: backup.sh [--efi-boot EFI_PARTITION] [-b|-r] backupfile --root root_partition'    
+    echo ' '
+    echo ' WARNING: root_partition must be already mounted for this script to work'
+    echo ' '
     exit 1
 }
 

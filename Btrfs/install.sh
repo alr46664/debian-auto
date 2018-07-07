619686facbd361d522643273636167a127ad4c27
Andre Madureira <andreluizromano@hotmail.com>
2018-07-06 23:13:18 -0300

    fix

--- Btrfs/install.sh
+++ Btrfs/install.sh
@@ -15,14 +15,9 @@ cd "${SCRIPT_DIR}/btrfsmaintenance" &&
 apt-get update && apt-get -y install btrfs-progs &&
 ./dist-install.sh &&
 install_service ${BTRFS_BALANCE}.service ${BTRFS_BALANCE}.timer ${BTRFS_SCRUB}.service ${BTRFS_SCRUB}.timer &&
-set_send_mail_on_failure ${BTRFS_BALANCE}.service ${BTRFS_SCRUB}.service &&
-set_timer_on_calendar ${BTRFS_BALANCE}.timer monthly ${BTRFS_SCRUB}.timer weekly &&
+systemctl daemon-reload &&
 systemctl enable ${BTRFS_BALANCE}.timer ${BTRFS_SCRUB}.timer
 STATUS=$?
-for var in ${BTRFS_BALANCE}.service ${BTRFS_SCRUB}.service; do
-    set_service_bg_priority "$var" 19
-    STATUS=$?
-done
 
 cd "$PWD"
 if [ $STATUS -eq 0 ]; then
